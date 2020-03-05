6728db8e3dacd1c5af4c2a43a4b4af59fab15f47
liaodehui <1959956996@qq.com>
2020-03-05 23:55:19 +0800

    feat 提醒记录

--- client/src/pages/rar/index.jsx
+++ client/src/pages/rar/index.jsx
@@ -1,16 +1,77 @@
-import { View, Text } from '@tarojs/components'
+import Taro, { Component } from '@tarojs/taro'
+import { View, Text, Image } from '@tarojs/components'
+import { AtIcon } from 'taro-ui'
+import { navigateTo } from '@utils/common'
+import './index.scss'
+import p5 from '@assets/images/p5.jpg'
+import ListItem from '@components/list-item'
 
-function Rar () {
-  return (
-    <View className='index'>
-      <Text>Hello world!</Text>
-    </View>
-  )
-}
+class Rar extends Component {
+  config = {
+    navigationBarBackgroundColor: '#fdd730',
+    navigationBarTitleText: '提醒记录'
+  }
+  state = {
+    index: 0
+  }
+  goto = (type) => {
+    if (this.state.index === 0) {
+      navigateTo(`/pages/${type}-reminder/index`)
+    }else {
+      navigateTo(`/pages/${type}-record/index`)
+    }
+  }
+  render() {
+    const { index } = this.state
 
-Rar.config = {
-  navigationBarBackgroundColor: '#fdd730',
-  navigationBarTitleText: '提醒记录'
+    return (
+      <View className='rar'>
+        <View className="rar-header">
+          <View className="rar-header-left">
+            <Text className="nickname">Coffee</Text>
+            <Text>
+              已陪伴小主<Text className="num">141</Text>天。
+            </Text>
+            <AtIcon prefixClass="icon" value="female" size="15" color="#000" />
+          </View>
+          <View className="rar-header-right">
+            <View className="avatar">
+              <Image src={p5} />
+            </View>
+          </View>
+        </View>
+        <View className="rar-content">
+          <View className="tab">
+            <Text 
+              className={ index === 0 ? 'active' : ''}
+              onClick={() => this.setState({ index: 0 })}
+            >提醒事项</Text>
+            <Text 
+              className={ index === 1 ? 'active' : ''}
+              onClick={() => this.setState({ index: 1 })}
+            >成长记录</Text>
+          </View>
+          {
+            index === 0 ? (
+              <View>
+                <ListItem />
+              </View>
+            ) : (
+              null
+            )
+          }
+        </View>
+        <View className="rar-oper">
+          <View onClick={() => this.goto('edit')}>
+            <AtIcon prefixClass="icon" value="edit" color="#FB6205" size="26"/>
+          </View>
+          <View onClick={() => this.goto('add')}>
+            <AtIcon prefixClass="icon" value="add" color="#f4ea2a" size="26"/>
+          </View>
+        </View>
+      </View>
+    )
+  }
 }
 
 export default Rar
