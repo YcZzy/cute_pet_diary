c141abdfec275ea35db7e9c7befaef2e91d6eb7a
liaodehui <1959956996@qq.com>
2020-03-07 18:46:10 +0800

    feat 各页面配置初始化

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,17 +1,30 @@
 import Taro, { Component } from '@tarojs/taro'
 import Index from './pages/index'
-import { Provider } from '@tarojs/redux'
+import { Provider, connect } from '@tarojs/redux'
 import configStore from './store'
 import './app.scss'
 import '@assets/styles/custom-variables.scss'
 import '@assets/styles/iconfont.scss'
+import { initRarReminder, initVariety, initRarRecord } from '@actions/config'
 
 const store = configStore()
+
+@connect(({}) => ({}), (dispatch) => ({
+  getConfig() {
+    dispatch(initVariety())
+    dispatch(initRarReminder())
+    dispatch(initRarRecord())
+  }
+}))
 class App extends Component {
   config = {
     pages: [
       
       'pages/pet/index',
+      'pages/add-record/index',
+      'pages/add-reminder/index',
+      'pages/add-pet/index',
+      'pages/rar/index',
       'pages/mine/index',
       'pages/index/index',
       'pages/explore/index',
@@ -20,9 +33,9 @@ class App extends Component {
       'pages/comment/index',
       'pages/fans/index',
       'pages/focus/index',
-      'pages/rar/index',
-      'pages/add-pet/index',
       'pages/pet-info/index',
+      'pages/edit-record/index',
+      'pages/edit-reminder/index'
     ],
     window: {
       backgroundTextStyle: 'light',
@@ -68,6 +81,7 @@ class App extends Component {
         traceUser: true // 是否要捕捉每个用户的访问记录。设置为true，用户可在管理端看到用户访问记录
       })
     }
+    this.props.getConfig()
   }
 
   // 在 App 类中的 render() 函数没有实际作用
