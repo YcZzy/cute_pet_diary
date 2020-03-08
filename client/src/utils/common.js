2b2a7a3829a2f3b744f7ff28b06eb99893506fa7
liaodehui <1959956996@qq.com>
2020-03-08 17:01:58 +0800

    feat switchTime

--- client/src/utils/common.js
+++ client/src/utils/common.js
@@ -148,3 +148,16 @@ export const computeFromArrivalDate = (arrival_date) => {
   }
   return sumDays
 }
+
+export const switchTime = (time) => {
+  if (typeof time === 'string') {
+    return new Date(time).getTime()
+  }else {
+    let date = new Date(time)
+    let ymd = date.toLocaleDateString().split('/').map(item => {
+      return item.length < 2 ? '0' + item : item
+    }).join('-')
+    let hm = date.toTimeString().substr(0, 5)
+    return `${ymd} ${hm}`
+  }
+}
