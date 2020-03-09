e7ba818aaae9321a6fb6d67e642711295e3c5a98
liaodehui <1959956996@qq.com>
2020-03-09 21:02:57 +0800

    update templateId

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -2,3 +2,4 @@ export const reminder_default_img = 'cloud://ldh-xinaa.6c64-ldh-xinaa-1259815928
 export const record__default_img = 'cloud://ldh-xinaa.6c64-ldh-xinaa-1259815928/CPD/rar_record/jilu_default.png'
 export const pet_avatar = 'CPD/pet_avatar/'
 export const record_picture = 'CPD/record_picture/'
+export const templateId = 'kqHePUKZxtp8JTF5AHvrCgAdYUib7u2pMsCdBdoD5GQ'
